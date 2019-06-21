66e3070017da94f5a5791e9a44f7dc49eed00455
1421806379 <1421806379@qq.com>
2019-06-21 15:11:27 +0800

    初次提交

---
+++ src/main/java/com/example/wx_pay/Controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.wx_pay.Controller;
+
+import com.example.wx_pay.Interface.WeixinPayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private WeixinPayService weixinPayService;
+
+    @RequestMapping("/createNative")
+    public Map createNative(){
+        return weixinPayService.createNative("463724632543","1");
+    }
+
+}
---
+++ src/main/java/com/example/wx_pay/Interface/WeixinPayService.java
@@ -0,0 +1,14 @@
+package com.example.wx_pay.Interface;
+
+import java.util.Map;
+
+public interface WeixinPayService {
+    /**
+     * 生成微信支付二维码
+     * @param out_trade_no 订单号
+     * @param total_fee 金额(分)
+     * @return
+     */
+    public Map createNative(String out_trade_no, String total_fee);
+
+}
